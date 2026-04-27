cf60918f7172f155cc728c436f684884f98628fd
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-04-27 21:25:52 +0200

    Redo exercise9 with functional reduce implementation

--- Excercise2/excercise9.js
+++ Excercise2/excercise9.js
@@ -1,13 +1,8 @@
 function calculateMatrixSum(matrix) {
-  let sum = 0;
-
-  for (let i = 0; i < matrix.length; i += 1) {
-    for (let j = 0; j < matrix[i].length; j += 1) {
-      sum += matrix[i][j];
-    }
-  }
-
-  return sum;
+  return matrix.reduce(
+    (sum, row) => sum + row.reduce((rowSum, value) => rowSum + value, 0),
+    0
+  );
 }
 
 console.log(calculateMatrixSum([[1, 2], [3, 4], [5, 6]]));
